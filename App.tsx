f0d52829794c1cf0ccddc3f0e20f989b4a85f0da
Ali Hassoun <alihassoun060@gmail.com>
2022-06-20 19:24:41 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("TEST");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
